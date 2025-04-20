b26c34eb2da05f3954d839b1e4d4ccf1c148d936
Randy-Chang <gdba5712@gmail.com>
2025-04-20 23:47:12 +0800

    調整Form的UI位置

--- Project_ImageViewer/Form1.Designer.cs
+++ Project_ImageViewer/Form1.Designer.cs
@@ -34,31 +34,34 @@
             this.plRoiImage = new System.Windows.Forms.Panel();
             this.btnMatch = new System.Windows.Forms.Button();
             this.btnShowMatchResult = new System.Windows.Forms.Button();
+            this.plOperate = new System.Windows.Forms.Panel();
+            this.plOperate.SuspendLayout();
             this.SuspendLayout();
             // 
             // plImageViewer
             // 
             this.plImageViewer.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.plImageViewer.Dock = System.Windows.Forms.DockStyle.Left;
+            this.plImageViewer.Dock = System.Windows.Forms.DockStyle.Fill;
             this.plImageViewer.Location = new System.Drawing.Point(0, 0);
             this.plImageViewer.Margin = new System.Windows.Forms.Padding(6);
             this.plImageViewer.Name = "plImageViewer";
-            this.plImageViewer.Size = new System.Drawing.Size(800, 600);
+            this.plImageViewer.Size = new System.Drawing.Size(882, 600);
             this.plImageViewer.TabIndex = 0;
             // 
             // splitter1
             // 
             this.splitter1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.splitter1.Location = new System.Drawing.Point(800, 0);
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.splitter1.Location = new System.Drawing.Point(882, 0);
             this.splitter1.Margin = new System.Windows.Forms.Padding(6);
             this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(18, 600);
+            this.splitter1.Size = new System.Drawing.Size(10, 600);
             this.splitter1.TabIndex = 1;
             this.splitter1.TabStop = false;
             // 
             // btnSaveRoiImage
             // 
-            this.btnSaveRoiImage.Location = new System.Drawing.Point(827, 318);
+            this.btnSaveRoiImage.Location = new System.Drawing.Point(21, 319);
             this.btnSaveRoiImage.Name = "btnSaveRoiImage";
             this.btnSaveRoiImage.Size = new System.Drawing.Size(400, 52);
             this.btnSaveRoiImage.TabIndex = 2;
@@ -68,14 +71,14 @@
             // plRoiImage
             // 
             this.plRoiImage.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.plRoiImage.Location = new System.Drawing.Point(827, 12);
+            this.plRoiImage.Location = new System.Drawing.Point(21, 13);
             this.plRoiImage.Name = "plRoiImage";
             this.plRoiImage.Size = new System.Drawing.Size(400, 300);
             this.plRoiImage.TabIndex = 3;
             // 
             // btnMatch
             // 
-            this.btnMatch.Location = new System.Drawing.Point(827, 397);
+            this.btnMatch.Location = new System.Drawing.Point(21, 377);
             this.btnMatch.Name = "btnMatch";
             this.btnMatch.Size = new System.Drawing.Size(400, 52);
             this.btnMatch.TabIndex = 4;
@@ -84,28 +87,38 @@
             // 
             // btnShowMatchResult
             // 
-            this.btnShowMatchResult.Location = new System.Drawing.Point(827, 527);
+            this.btnShowMatchResult.Location = new System.Drawing.Point(21, 435);
             this.btnShowMatchResult.Name = "btnShowMatchResult";
             this.btnShowMatchResult.Size = new System.Drawing.Size(400, 52);
             this.btnShowMatchResult.TabIndex = 5;
             this.btnShowMatchResult.Text = "Show Match Result";
             this.btnShowMatchResult.UseVisualStyleBackColor = true;
             // 
+            // plOperate
+            // 
+            this.plOperate.Controls.Add(this.btnShowMatchResult);
+            this.plOperate.Controls.Add(this.plRoiImage);
+            this.plOperate.Controls.Add(this.btnMatch);
+            this.plOperate.Controls.Add(this.btnSaveRoiImage);
+            this.plOperate.Dock = System.Windows.Forms.DockStyle.Right;
+            this.plOperate.Location = new System.Drawing.Point(892, 0);
+            this.plOperate.Name = "plOperate";
+            this.plOperate.Size = new System.Drawing.Size(546, 600);
+            this.plOperate.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1438, 600);
-            this.Controls.Add(this.btnShowMatchResult);
-            this.Controls.Add(this.btnMatch);
-            this.Controls.Add(this.plRoiImage);
-            this.Controls.Add(this.btnSaveRoiImage);
-            this.Controls.Add(this.splitter1);
             this.Controls.Add(this.plImageViewer);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.plOperate);
             this.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.plOperate.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -118,6 +131,7 @@
         private System.Windows.Forms.Panel plRoiImage;
         private System.Windows.Forms.Button btnMatch;
         private System.Windows.Forms.Button btnShowMatchResult;
+        private System.Windows.Forms.Panel plOperate;
     }
 }
 
